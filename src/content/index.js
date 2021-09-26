48d1dea6d07c73e73e7187b76342cd7972f668bd
Kiwi Li <tjliqy@163.com>
2021-09-27 02:01:45 +0800

    2

--- src/content/index.js
+++ src/content/index.js
@@ -4,10 +4,25 @@
 // let current_user =document.getElementsByClassName('name')[0]["childNodes"][0]
 let current_user = document.getElementsByClassName('user-icon')[1]['children'][0]['children'][1]['childNodes'][0].data
 console.log('获取到的元素内容为', current_user)
+// let tempNode = document.createElement('iframe');
+// console.log(1,tempNode)
+// tempNode.setAttribute('src','popup.html');
+// console.log(tempNode)
+// document.body.append(tempNode);
+
 chrome.runtime.sendMessage({
     from: 'content',
     info: current_user
 }, res => {
     // 答复
-    // console.log('content: receive success', res)
+    console.log('content: receive success', res)
 })
+
+
+let newsListDom = document.querySelector("#root > div > div.main-content > div.left-container > div > div > div:nth-child(2)")
+for (let i = 0; i < newsListDom.children.length; i++) {
+    newsListDom.children[i].getElementsByTagName('a')[0].onclick(function (){
+        console.log('clicked')
+    })
+}
+
--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="main_app">
-    <div class="panel-background">
-      <div class="top-icon">
-        <a>—</a>
-      </div>
+    <div class="panel-background" v-loading="loading">
+<!--      <div class="top-icon">-->
+<!--        <a>—</a>-->
+<!--      </div>-->
       <div class="top-title">
         <p>新闻算法测评</p>
       </div>
@@ -11,8 +11,8 @@
         <el-divider class="divider" />
       </div>
       <div class="user-info">
-        <b style="float: left">当前用户：{{ activeUser.name }}</b>
-        <b style="float: right ">历史浏览量：{{history.length}}</b>
+        <b style="float: left">当前用户：{{ username }}</b>
+        <b style="float: right ">历史浏览量：{{newsListLength}}</b>
       </div>
       <div class="category-panel">
         <b class="category-title">最常浏览种类</b>
@@ -44,7 +44,7 @@
 <script>
 import CategoryChart from "../../components/user/CategoryChart";
 import ModelInfo from "../../components/model/ModelInfo";
-import HistoryCard from "@/components/user/HistoryCard";
+import HistoryCard from "../../components/user/HistoryCard";
 
 export default {
   name: "app",
@@ -61,29 +61,25 @@ export default {
   data() {
     return {
       // 模拟数据 未进行前后端用户连接
-      activeUser: {
-        id: "1",
-        name: "Jack",
-        sex: "男",
-        age: 30,
-        location: "LA",
-        history: ['6993568771491299878', '7005501448200913445', '7005398379866063396', '6998537592664900108', '6988813649813160455']
-      },
+      username:"",
       category: "Sport",
       historyNewsList:[],
       categoryCount: {},
       newsIndex:0,
       baseMetrics: {},
       metrics: {},
-      robust: {}
+      robust: {},
+      loading:true,
+      newsListLength: 0
     };
   },
   methods: {
     loadUserId() {
+      this.loading = true
       let vue_this = this;
       let win = chrome.extension.getBackgroundPage();
       console.log("###########################", win.data);
-      vue_this.activeUser.name = win.data;
+      vue_this.username = win.data;
       // getchrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
       //   console.log("$$$$$$$$$$$$$$$$$", req);
       //   if (req.from == "background") {
@@ -94,19 +90,20 @@ export default {
       // });
       this.axios
         .post("http://172.31.128.207:8082/chrome-extension/userinfo", {
-          username: this.activeUser.name
+          username: this.username
         })
         .then(res => {
           let result = res.data;
           if (result && result.status === 200) {
             this.loadNews(result.data['news_id'])
-
           } else {
             console.log("获取用户数据失败");
+            this.loading = false
           }
         });
     },
     loadNews(history) {
+      this.loading = true
       this.axios
         .post("http://172.31.128.207:8082/chrome-extension/findByIdList", {
           newsIdList: history
@@ -114,16 +111,18 @@ export default {
         .then(res => {
           let data = res.data.data;
           this.categoryCount = data.categoryCount;
-          console.log(data)
-          this.newsList = data.newsList
-          this.newsIndex = 0
-          this.loadHistory()
+          this.newsList = data.newsList;
+          this.newsIndex = 0;
+          this.newsListLength = this.newsList.length
+          this.loadHistory();
           this.loadCategoryRank(data.categoryCount, "category");
+          this.loading = false
+
         });
     },
     loadHistory(){
-      let gap = 5
-      let length = this.newsList.length
+      let gap = 5;
+      let length = this.newsList.length;
       if (length - this.newsIndex >= gap){
         this.historyNewsList = this.historyNewsList.concat(this.newsList.slice(length - this.newsIndex - gap, length- this.newsIndex))
       }else if(length - this.newsIndex > 0) {
@@ -199,7 +198,7 @@ export default {
   border: 2px solid maroon;
   background-color: #f6f6f6;
   border-radius: 10px;
-  overflow: scroll;
+  /*overflow: scroll;*/
 }
 
 .top-icon {
